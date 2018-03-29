cc8a0ee989161148f07ce0522b04231ed5148747
faisaltheparttimecoder <fai28683@gmail.com>
2018-03-29 14:03:00 +0100

    Changes to the datatype of the tables
    Increased datatype size to accomdate more data

--- zendesk/models.py
+++ zendesk/models.py
@@ -7,9 +7,9 @@ class Organisation(models.Model):
     Comment: The place to store all the Organization
     """
     org_id = models.BigIntegerField(null=False, unique=True)
-    name = models.CharField(max_length=100, null=False, unique=True)
+    name = models.CharField(max_length=200, null=False, unique=True)
     created_at = models.DateField()
-    location = models.CharField(max_length=20)
+    location = models.CharField(max_length=20, default='')
     expired_contract = models.BooleanField(default=False)
 
     class Meta:
@@ -39,9 +39,9 @@ class TicketNote(models.Model):
     """
     ticket_id = models.BigIntegerField(null=False, db_index=True)
     org_id = models.ForeignKey(Organisation, related_name='org', on_delete=models.CASCADE)
-    author = models.CharField(max_length=30, null=False, default="Unknown")
+    author = models.CharField(max_length=50, null=False, default="Unknown")
     created = models.DateTimeField(auto_now=True, null=False)
-    notes = models.CharField(max_length=300)
+    notes = models.CharField(max_length=500)
 
     def __str__(self):
         return str(self.ticket_id)
